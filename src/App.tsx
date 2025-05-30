49efa1ac755a28934ed921f6b0f57f13de6dd4ca
Pham Minh Triet <vodanhtinh203@gmail.com>
2025-05-30 10:28:41 +0700

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { APITester } from "./APITester";
 import "./index.css";
 
-
 import logo from "./logo.svg";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import reactLogo from "./react.svg";
 
 export function App() {
-  return (
-    <Login />
-  );
+  return <Login />;
 }
 
 export default App;
